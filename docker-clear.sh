67717d5e2b25632aa9e4dacba51ab51188114be5
karthikeya0800 <karthikeya0800@gmail.com>
2024-09-23 08:04:46 +0530

    added title to env

--- docker-clear.sh
+++ docker-clear.sh
@@ -2,6 +2,7 @@
 docker rm $(docker ps -a -q) -f > /dev/null 2>&1
 docker rmi $(docker images -q) > /dev/null 2>&1
 docker volume rm $(docker volume ls) > /dev/null 2>&1
+docker system prune -af --volumes
 docker network prune -f > /dev/null 2>&1
 docker images
 docker ps -a
